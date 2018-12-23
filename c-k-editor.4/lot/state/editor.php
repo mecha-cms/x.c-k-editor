c10f515202afebb862a83ad99e149ce10ef7a895
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-12-23 22:38:31 +0700

    Update editor.php
    https://github.com/mecha-cms/extend.c-k-editor/issues/4

--- c-k-editor.4/lot/state/editor.php
+++ c-k-editor.4/lot/state/editor.php
@@ -1,35 +1,18 @@
 <?php
 
 return [
-    'customConfig' => false,
-    'stylesSet' => false,
     'filebrowserWindowWidth' => 600,
     'filebrowserWindowHeight' => 300,
-    'toolbarGroups' => [
-        [
-            'name' => 'styles',
-            'groups' => ['styles']
-        ], [
-            'name' => 'basicstyles',
-            'groups' => ['basicstyles']
-        ], [
-            'name' => 'links',
-            'groups' => ['links']
-        ], [
-            'name' => 'paragraph',
-            'groups' => ['list', 'blocks']
-        ], [
-            'name' => 'insert',
-            'groups' => ['insert']
-        ], [
-            'name' => 'clipboard',
-            'groups' => ['undo']
-        ], [
-            'name' => 'data',
-            'groups' => ['mode']
-        ]
+    'toolbar' => [
+        ['Format'],
+        ['Bold', 'Italic' /* , 'Code', 'Underline', 'Strike', 'Subscript', 'Superscript' */ ],
+        ['Link', 'Unlink'],
+        ['NumberedList', 'BulletedList'],
+        ['Blockquote'],
+        ['CreatePlaceholder', 'Image', 'Table' /* , 'HorizontalRule' */ ],
+        ['Undo', 'Redo'],
+        ['Sourcedialog']
     ],
-    'removeButtons' => 'Anchor,Copy,Cut,Indent,Outdent,Paste,Strike,Subscript,Superscript,Underline',
     // For semantic HTML structure, we use `<h3>` as the largest heading level
     // because we already have `<h1>` as the site title and `<h2>` as the page title
     'format_tags' => 'p;h3;h4;h5;h6;pre',
@@ -38,5 +21,5 @@ return [
     'linkShowAdvancedTab' => false,
     'removeDialogFields' => 'table:info:txtBorder;table:info:cmbAlign;table:info:txtCellSpace;table:info:txtCellPad',
     'disallowedContent' => 'article aside base body footer head header hgroup html iframe link main nav noscript script style title',
-    'extraPlugins' => 'confighelper,placeholder,sourcedialog'
+    'extraPlugins' => 'codetag,confighelper,horizontalrule,sourcedialog'
 ];
