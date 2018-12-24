9e0b58edbe03a449377a166b4c55a79fc17052fa
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-12-24 14:04:51 +0700

    TODO: Translation

--- c-k-editor/_index.php
+++ c-k-editor/_index.php
@@ -4,7 +4,7 @@ $t = filemtime(__FILE__);
 $state = Extend::state('panel');
 Asset::set(__DIR__ . DS . 'lot' . DS . 'asset' . DS . 'css' . DS . 'content.min.css', 10);
 Asset::set(__DIR__ . DS . 'lot' . DS . 'worker' . DS . '@ckeditor' . DS . 'ckeditor.js', 10);
-Asset::set($url . '/' . $state['path'] . '/::g::/-/c-k-editor.min.js', 10.1, [
+Asset::set($url . '/' . $state['path'] . '/::g::/-/c-k-editor.min.js', 10.2, [
     'src' => function($src) use($config, $t, $user) {
         $t += filemtime(__DIR__ . DS . 'lot' . DS . 'worker' . DS . 'worker' . DS . 'route.php');
         $t += filemtime(__DIR__ . DS . 'lot' . DS . 'asset' . DS . 'js' . DS . basename($src));
@@ -12,3 +12,7 @@ Asset::set($url . '/' . $state['path'] . '/::g::/-/c-k-editor.min.js', 10.1, [
         return candy($this->url, [$src, abs(crc32($t . $user->token . $config->language))]);
     }
 ]);
+
+if ($f = File::exist(__DIR__ . DS . 'lot' . DS . 'worker' . DS . '@ckeditor' . DS . 'translations' . DS . explode('-', $config->language)[0] . '.js')) {
+    Asset::set($f, 10.1);
+}
